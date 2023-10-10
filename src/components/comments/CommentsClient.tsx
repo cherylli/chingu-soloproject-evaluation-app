63cfc201e97e0f0c33f03bef77bfafbef6375aea
Cheryl M <webmaster@cherylli.com>
2023-10-10 21:33:59 +1100

    move comment elements around

--- src/components/comments/CommentsClient.tsx
+++ src/components/comments/CommentsClient.tsx
@@ -1,13 +1,15 @@
 'use client'
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import CommentItem from "@/components/comments/CommentItem";
 import {Comment} from "@/types/CommentTypes";
 import {Collapsible, CollapsibleContent, CollapsibleTrigger} from "@/components/ui/collapsible";
 import CommentForm from "@/components/comments/CommentForm";
 import {addCommentByRecordId} from "@/services/comments";
+import {ChevronDown, ChevronUp} from "lucide-react";
 
 const CommentsClient = ({recordId, comments}: { recordId: string, comments: Comment[] }) => {
     const [localComments, setLocalComments] = useState<Comment[]>(comments)
+    const [isOpen, setIsOpen] = useState(true)
 
     const handleAddComment = async (newComment:string) => {
         const res = await addCommentByRecordId(recordId, newComment)
@@ -18,8 +20,12 @@ const CommentsClient = ({recordId, comments}: { recordId: string, comments: Comm
 
     return (
         <div>
-            <Collapsible className="m-5 ">
-                <CollapsibleTrigger>{`Comments (${localComments.length})`}</CollapsibleTrigger>
+            <Collapsible className="m-5" open={isOpen} onOpenChange={setIsOpen}>
+                <CommentForm handleAddComment={handleAddComment}/>
+                <CollapsibleTrigger className="flex items-end">
+                    {`Comments (${localComments.length})`}
+                    {isOpen?<ChevronUp/>:<ChevronDown/>}
+                </CollapsibleTrigger>
                 <CollapsibleContent>
                     {localComments ?
                         localComments.map(comment => (
@@ -27,7 +33,6 @@ const CommentsClient = ({recordId, comments}: { recordId: string, comments: Comm
                         ) : null
                     }
                 </CollapsibleContent>
-                <CommentForm handleAddComment={handleAddComment}/>
             </Collapsible>
         </div>
     )
