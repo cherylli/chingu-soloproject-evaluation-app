ccd1201b969a77d91e65dff0b2870ad766b43d3b
Cheryl M <webmaster@cherylli.com>
2024-04-19 12:07:12 +1000

    fix type error

--- src/types/Answer.ts
+++ src/types/Answer.ts
@@ -9,7 +9,8 @@ export type Answer = {
 // mapped questions combines userAnswers + original questions information from the JSON file
 // we use this in PO and SM detail pages to map through all the questions
 export type MappedQuestionAndAnswer = {
-    question: Answer[],
+    questionNumber: string,
+    question: string,
     answer: string[],
     options: string,
     userAnswer: any,
