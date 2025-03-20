c86b5ad547dd65a2822b0cce296044dbcde12467
Cheryl M <webmaster@cherylli.com>
2025-03-20 23:31:30 +1100

    update pass message

--- src/lib/getRandomPassMessage.ts
+++ src/lib/getRandomPassMessage.ts
@@ -3,5 +3,5 @@ import {emojis} from "@/data/discordEmoji";
 
 export const getRandomPassMessage = (discordName: string) => {
     return `${messages[Math.floor(Math.random() * messages.length)].replace('{name}', discordName)} `+
-        `${emojis[Math.floor(Math.random() * emojis.length)]}`
+        `${emojis[Math.floor(Math.random() * emojis.length)]} \n\nWe have DM'd you your feedback. Please also check your message requests or spam.`
 }
