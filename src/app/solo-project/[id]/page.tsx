31560e7c3e6b36d7f8b9cc7475bc4bc597a98c21
Cheryl M <webmaster@cherylli.com>
2025-04-23 23:13:03 +1000

    update solo project page panel to resizable using shadcn

--- src/app/solo-project/[id]/page.tsx
+++ src/app/solo-project/[id]/page.tsx
@@ -6,11 +6,12 @@ import {
     updateSoloProjectById
 } from "@/services/soloProjects";
 import ProjectSubmissionDetail from "@/components/soloprojects/details/BaseDetails";
-import {ActionResponse} from "@/types";
+import { ActionResponse } from "@/types";
 import Comments from "@/components/comments";
 import FeedbackContainer from "@/components/feedback/FeedbackContainer";
 import CompactList from "@/components/soloprojects/CompactList";
 import ReadOnly from "@/components/soloprojects/details/ReadOnly";
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
 
 const SoloProjectPage = async (props: { params: Promise<{ id: string }> }) => {
     const params = await props.params;
@@ -43,21 +44,26 @@ const SoloProjectPage = async (props: { params: Promise<{ id: string }> }) => {
         record.fields["Evaluation Status"] === "Passed" ?
             <ReadOnly record={record}
                       handleStatusChange={handleStatusChange}/> :
-            <div className="flex flex-col lg:flex-row-reverse justify-between h-[calc(100vh-110px)]">
-                <div className="lg:w-1/2 lg:overflow-y-auto">
-                    {projects.length > 1 && <CompactList records={projects}/>}
-                    <ProjectSubmissionDetail
-                        record={record}
-                        handleSave={handleSave}
-                        handleSetEvaluator={handleSetEvaluator}
-                        handleRemoveEvaluator={handleRemoveEvaluator}
-                    />
-                    <Comments recordId={params.id}/>
-                </div>
-                <div className="hidden lg:block lg:w-1/2 lg:max-h-screen lg:overflow-y-auto">
-                    <FeedbackContainer discordName={record.fields["Discord Name"]}/>
-                </div>
-            </div>
+            <ResizablePanelGroup direction="horizontal">
+                <ResizablePanel defaultSize={50}>
+                    <div className="hidden lg:block lg:overflow-y-auto lg:max-h-screen">
+                        <FeedbackContainer discordName={record.fields["Discord Name"]}/>
+                    </div>
+                </ResizablePanel>
+                <ResizableHandle/>
+                <ResizablePanel defaultSize={50}>
+                    <div className="lg:overflow-y-auto">
+                        {projects.length > 1 && <CompactList records={projects}/>}
+                        <ProjectSubmissionDetail
+                            record={record}
+                            handleSave={handleSave}
+                            handleSetEvaluator={handleSetEvaluator}
+                            handleRemoveEvaluator={handleRemoveEvaluator}
+                        />
+                        <Comments recordId={params.id}/>
+                    </div>
+                </ResizablePanel>
+            </ResizablePanelGroup>
     )
 }
 
