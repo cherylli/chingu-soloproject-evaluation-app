b22cee9c087e35c18a6d40f37700b7c002f1c8a6
Cheryl M <webmaster@cherylli.com>
2025-08-11 09:51:08 +1000

    add UI/UX and Voyage Guide roles with corresponding styles

--- src/components/voyages/signups/singleVoyageColumnDef.tsx
+++ src/components/voyages/signups/singleVoyageColumnDef.tsx
@@ -1,50 +1,48 @@
-import {createColumnHelper} from "@tanstack/table-core";
-import {VoyageSignup} from "@/types/VoyageSignupTypes";
-import {SiAirtable} from "@icons-pack/react-simple-icons";
-import {roleColors} from "@/styles/roles";
+import { roleColors } from '@/styles/roles';
+import { VoyageSignup } from '@/types/VoyageSignupTypes';
+import { SiAirtable } from '@icons-pack/react-simple-icons';
+import { createColumnHelper } from '@tanstack/table-core';
 
-const columnHelper = createColumnHelper<VoyageSignup>()
+const columnHelper = createColumnHelper<VoyageSignup>();
 
-export const singleVoyageColumnDef =(
-    baseURL: string
-) => [
-    columnHelper.accessor((row) => row.fields["Team No."], {
-        id: "teamNum",
-        header: "Team",
-        enableGrouping: true,
-        enableSorting: true,
-        getGroupingValue: (row) =>
-            `${row.fields["Team Name"]} - ${row.fields["Team No."] || "no team"}`,
-        // TODO: show how many active in the team
-    }),
-    columnHelper.display({
-        id: "Links",
-        cell: ({row}) => <div>
-            <a href={`${baseURL}/${row.original.id}`} target="_blank" rel="noreferrer" >
-                <SiAirtable/>
-            </a>
-        </div>
+export const singleVoyageColumnDef = (baseURL: string) => [
+  columnHelper.accessor((row) => row.fields['Team No.'], {
+    id: 'teamNum',
+    header: 'Team',
+    enableGrouping: true,
+    enableSorting: true,
+    getGroupingValue: (row) =>
+      `${row.fields['Team Name']} - ${row.fields['Team No.'] || 'no team'}`,
+    // TODO: show how many active in the team
+  }),
+  columnHelper.display({
+    id: 'Links',
+    cell: ({ row }) => (
+      <div>
+        <a href={`${baseURL}/${row.original.id}`} target="_blank" rel="noreferrer">
+          <SiAirtable />
+        </a>
+      </div>
+    ),
+  }),
+  columnHelper.accessor((row) => row.fields['Discord Name'], {
+    header: 'Discord Name',
+  }),
+  columnHelper.accessor((row) => row.fields['GitHub ID'], {
+    header: 'Github Id',
+  }),
+  columnHelper.accessor((row) => row.fields['Status'], {
+    header: 'Status',
+  }),
+  columnHelper.accessor((row) => row.fields['Role'], {
+    header: 'Role',
+    cell: ({ row }) => {
+      const classColor =
+        row.original.fields.Status === 'Active'
+          ? roleColors[row.original.fields['Role']]?.text
+          : '';
 
-    }),
-    columnHelper.accessor((row) => row.fields["Discord Name"], {
-        header: "Discord Name",
-    }),
-    columnHelper.accessor((row) => row.fields["GitHub ID"], {
-        header: "Github Id",
-    }),
-    columnHelper.accessor((row) => row.fields["Status"], {
-        header: "Status",
-    }),
-    columnHelper.accessor((row)=>row.fields["Role"],{
-        header: "Role",
-        cell: ({row}) => {
-            const classColor = row.original.fields.Status === "Active"
-                ? roleColors[row.original.fields["Role"]].text
-                : ""
-
-            return <span className={classColor}>{row.original.fields["Role"]}</span>
-
-
-        }
-    })
-]
+      return <span className={classColor}>{row.original.fields['Role']}</span>;
+    },
+  }),
+];
