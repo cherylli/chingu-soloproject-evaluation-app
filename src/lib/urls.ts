538d01a2c4e7c607890bee2bbed0ba70a9f28786
Cheryl M <webmaster@cherylli.com>
2025-09-18 21:04:54 +1000

    fix: [SPA-52](voyage signup) fix voyage team github url

--- src/lib/urls.ts
+++ src/lib/urls.ts
@@ -1,13 +1,16 @@
-import {VoyageSignupFields} from "@/types/VoyageSignupTypes";
+import { VoyageSignupFields } from '@/types/VoyageSignupTypes';
 
 export const githubTeamUrl = (
-     voyageSignupFields: VoyageSignupFields
-    // voyageNum: string,
-    // tierNum: "1" | "2" | "3",
-    // teamNum: string
+  voyageSignupFields: VoyageSignupFields
+  // voyageNum: string,
+  // tierNum: "1" | "2" | "3",
+  // teamNum: string
 ) => {
-    const voyageNum = voyageSignupFields?.Voyage.slice(1) || "0"
-    const tierNum = voyageSignupFields?.Tier[5] || "0"
-    const teamNum = voyageSignupFields["Team No."]?.padStart(2,'0') || "0"
-    return `https://github.com/orgs/chingu-voyages/teams/v${voyageNum}-tier${tierNum}-team-${teamNum}/members`
-}
+  const voyageNum =
+    voyageSignupFields?.Voyage.slice(1) || '0';
+  const tierNum =
+    voyageSignupFields?.['Team Name'][5] || '0';
+  const teamNum =
+    voyageSignupFields['Team No.']?.padStart(2, '0') || '0';
+  return `https://github.com/orgs/chingu-voyages/teams/v${voyageNum}-tier${tierNum}-team-${teamNum}/members`;
+};
