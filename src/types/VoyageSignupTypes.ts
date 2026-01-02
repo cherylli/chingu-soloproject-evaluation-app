050bd8780698eca7d7fb1935b0c5ebd6fc214940
Cheryl Murphy <webmaster@cherylli.com>
2026-01-02 20:30:38 +1100

    chore: add extra fields for voyagesignup types

--- src/types/VoyageSignupTypes.ts
+++ src/types/VoyageSignupTypes.ts
@@ -1,38 +1,45 @@
-import {SoloProjectTier, VoyageRole} from "@/types/SoloProjectTypes";
+import {
+  SoloProjectTier,
+  VoyageRole,
+} from '@/types/SoloProjectTypes';
 
 export type VoyageSignup = {
-    id: string,
-    fields: VoyageSignupFields
-}
+  id: string;
+  fields: VoyageSignupFields;
+};
 
 export type VoyageSignupFields = {
-    Timestamp: string,
-    Email: string,
-    "Discord Name": string,
-    "GitHub ID": string,
-    "Evaluation Status (from Solo Project Link)": string,
-    "Status": string,
-    "Status Comment": string,
-    "Voyage": string,
-    "Team Name": string, // Tier generated google form automation
-    "Team No.": string,
-    "Timezone": string,
-    "Role": VoyageRole,
-    "Role Type": string,
-    "Tier": SoloProjectTier, // original signup tier?
-    "Info to Share": string,
-    "Application Link": string, // might not need -> fetch in user instead
-    "Solo Project Link": string, // might not need  -> fetch in user instead
-    "Confirmation Form Completed": boolean,
-    "Showcase Name Permission?": boolean,
-    "Discord ID": string, // might not need -> fetch in user instead
-    "Product (from Most Recent Subscriptions & Product Sales)": string,
-}
+  Timestamp: string;
+  Email: string;
+  'Discord Name': string;
+  'GitHub ID': string;
+  'Evaluation Status (from Solo Project Link)': string;
+  Status: string;
+  'Status Comment': string;
+  Voyage: string;
+  'Team Name': string; // Tier generated google form automation
+  'Team No.': string;
+  Timezone: string;
+  Role: VoyageRole;
+  'Role Type': string;
+  Tier: SoloProjectTier; // original signup tier?
+  'Info to Share': string;
+  'Application Link': string; // might not need -> fetch in user instead
+  'Solo Project Link': string; // might not need  -> fetch in user instead
+  'Confirmation Form Completed': boolean;
+  'Showcase Name Permission?': boolean;
+  'Discord ID': string; // might not need -> fetch in user instead
+  'Product (from Most Recent Subscriptions & Product Sales)': string;
+  // check solo project tier
+  'Solo Project Exceptions': string;
+  'Solo Project Tier (Lookup)': string; // should match "Team Name"
+};
 
 // Search Fields
 const voyageSignupSearchableFields = [
-    "Discord ID",
-    "Email"
+  'Discord ID',
+  'Email',
 ] as const satisfies readonly (keyof VoyageSignupFields)[];
 
-export type VoyageSignupSearchableFields = typeof voyageSignupSearchableFields[number]
+export type VoyageSignupSearchableFields =
+  (typeof voyageSignupSearchableFields)[number];
